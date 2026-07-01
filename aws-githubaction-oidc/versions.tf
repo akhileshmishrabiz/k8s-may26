ed28fce03d6a42de31fac77d718eda95175437ee
Devops with Akhilesh <livingdevops@gmail.com>
2026-07-01 22:39:03 +0530

    aws github oidc terraform

---
+++ aws-githubaction-oidc/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "1.12.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket       = "state-bucket-879381241087"
+    key          = "eks-may-2026/oidc-githubaction/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
